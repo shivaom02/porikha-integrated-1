ef31aef92cd09e2eaff88b691870413dd8302b1b
Shivam Kumar <67912385+shivaom02@users.noreply.github.com>
2021-03-20 13:14:43 +0530

    required modules added externally

--- client/client/src/components/exam/examCSS.js
+++
@@ -1,8 +0,0 @@
-export const formContainer = {
-    height:'120px',
-    width:'250px',
-    display:'flex',
-    flexDirection:'column',
-    justifyContent:'space-around',
-    allignItems:'center'
-}
--- client/client/src/context/webcamContext/webcamContext.js
+++
@@ -1,5 +0,0 @@
-import {createContext} from "react";
-
-const webCamContext=createContext();
-
-export default webCamContext;
